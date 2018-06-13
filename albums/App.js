8ce5e0857458913dc005c6e32ac3032174d8875b
Shai Alaloof <shaialaloof@Shais-MacBook-Pro.local>
2018-06-13 17:05:03 +1000

    Up to next module, completing AJAX requests.

--- albums/App.js
+++ albums/App.js
@@ -2,12 +2,16 @@
 
 // Import a library to  help create a Component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header.js';
+import AlbumList from './src/components/AlbumList.js';
 
 // Create a component
 const App = () => (
-  <Header headerText={'Albums'} />
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 // Render it to the device
 AppRegistry.registerComponent('albums', () => App);
